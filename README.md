de8f2c76c5b98343155c73c75446cfa5b1f60561
luferrduc <lu.ferrando@duocuc.cl>
2024-10-28 21:00:31 -0300

    Add: envinronment variables

--- README.md
+++ README.md
@@ -4,7 +4,18 @@
 
 # Teslo API
 
-1. Levantar base de datos
+1. Clonar proyecto
+2. Instalar dependencias
+``` bash
+yarn install
 ```
+3. Clonar archivo __env.template__ y renombrarlo a __.env__
+4. Cambiar las variables de entorno
+5. Levantar base de datos
+```bash
 docker compose up -d
+```
+6. Levantar en modo desarrollo
+```bash
+yarn start:dev
 ```
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      // Cuando se crean cambios en las entidades, se sincroniza automaticamente
+      // No se usa normalmente en producción
+      synchronize: true 
+    }),
+    ProductsModule
+  ],
   controllers: [],
   providers: [],
 })
